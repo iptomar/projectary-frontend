b8fd54e0ca3a4950df6ae187ad51f01536bf43d8
luisnunes111 <luisnunes111@hotmail.com>
2017-03-26 23:41:36 +0100

    routes adicionadas

---
+++ projectaryApp/app/projects/project-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "project-list",
+	templateUrl: "app/projects/project-list.component.html",
+    styleUrls:["app/projects/project-list.component.css"]
+})
+ 
+export class ProjectListComponent{
+
+
+}
---
+++ projectaryApp/app/projects/project.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "project",
+	templateUrl: "app/projects/project.component.html",
+    styleUrls:["app/projects/project.component.css"]
+})
+ 
+export class ProjectComponent{
+
+
+}
