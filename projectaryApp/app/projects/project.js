dc1d3d827eb42bfbbe4a9c48ae09a0a4cf38f3ad
rbarcelos93 <aluno18834@ipt.pt>
2017-03-28 12:07:44 +0100

    Atualização

--- projectaryApp/app/projects/project.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//state(Por atribuir, atribuido, cancelado), title, releaseDate, attributionDate, Orientador??? 
-//# sourceMappingURL=project.js.map
