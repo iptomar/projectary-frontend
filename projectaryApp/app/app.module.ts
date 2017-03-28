ea533185e510d79c34ebe4e7754e69cebf567714
luisnunes111 <luisnunes111@hotmail.com>
2017-03-28 04:09:10 +0100

    update dos projects(pipe, html, service)

--- projectaryApp/app/app.module.ts
+++ projectaryApp/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { ProjectComponent } from "./projects/project-detail.component";
 import { ProjectListComponent } from "./projects/project-list.component";
-import { RouterModule } from "@angular/router";
+import { ProjectService } from "./projects/project.service";
+import { ProjectFilterPipe } from "./projects/project-filter.pipe";
+
 
 @NgModule({
   imports: [ 
     BrowserModule,
+    FormsModule,
+    HttpModule,
     RouterModule.forRoot([
       {path: '',component: HomeComponent},
       {path: 'home',component: HomeComponent},
@@ -18,7 +25,14 @@ import { RouterModule } from "@angular/router";
     ])
   ], 
   declarations: [ 
-    AppComponent,HomeComponent,ProjectComponent,ProjectListComponent
+    AppComponent,
+    HomeComponent,
+    ProjectComponent,
+    ProjectListComponent,
+    ProjectFilterPipe
+  ],
+  providers: [
+      ProjectService
   ],
   bootstrap: [ AppComponent ]
 })
--- projectaryApp/app/projects/project.service.js
+++ projectaryApp/app/projects/project.service.js
@@ -18,12 +18,12 @@ require("rxjs/add/operator/catch");
 var ProjectService = (function () {
     function ProjectService(_http) {
         this._http = _http;
-        this._productUrl = "./projects.json";
+        this._productUrl = "app/projects/projects.json";
     }
     ProjectService.prototype.getProducts = function () {
-        return this._http.get(this._productUrl).map(function (response) { return response.json(); })
-            .do(function (data) { return console.log("All: " + JSON.stringify(data)); })
-            .catch(this.handleError);
+        return this._http.get(this._productUrl).map(function (response) { return response.json(); });
+        // .do(data => console.log("All: " + JSON.stringify(data)))
+        // .catch(this.handleError);
     };
     ProjectService.prototype.handleError = function (error) {
         console.error(error);
