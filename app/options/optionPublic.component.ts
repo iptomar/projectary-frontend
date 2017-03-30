9b8590f48580e1f6072ff07fc120e783b5dfd58d
rbarcelos93 <aluno18834@ipt.pt>
2017-03-30 16:10:23 +0100

    Faltou Não sei por que faltou o commit

---
+++ app/options/optionPublic.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "option-public",
+    templateUrl: "app/options/optionPublic.component.html"
+})
+
+export class OptionPublicComponent{
+}
