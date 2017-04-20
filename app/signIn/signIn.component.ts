fa9c1ed6826d274179aa841b39b09c1721ff99b3
Rui Barcelos <barcelos.rui@gmail.com>
2017-04-20 16:59:38 +0100

    Not used

--- app/signIn/signIn.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-	templateUrl: "app/signIn/signIn.component.html",
-})
-
-export class SignInComponent{
-  title = 'Registar Utilizador';
-}
