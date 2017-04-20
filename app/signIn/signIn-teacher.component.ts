fa1b581208b92e4d8c0b134e50de7a94eccc8332
florencioalex <florencioalex1993@gmail.com>
2017-04-20 16:34:57 +0100

    Separated from student and Teacher ALTER login HTML component Student ADD login HTML component Teacher Change routes

---
+++ app/signIn/signIn-teacher.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	templateUrl: "app/signIn/signIn-teacher.component.html",
+})
+
+export class SignInTeacherComponent{
+  title = 'Registar Professor';
+}
