43931e15ece303386d49ab082237a5b01ca7a3ea
diogosantosmendes <diogo.santos.mendes@hotmail.com>
2017-05-26 23:23:08 +0100

    remove JS from repo

--- src/app/projects/project.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Application = (function () {
-    function Application() {
-    }
-    return Application;
-}());
-exports.Application = Application;
-//# sourceMappingURL=project.js.map
