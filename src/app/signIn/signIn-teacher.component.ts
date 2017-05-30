b8fbb94e5a84a18f7a90f70641a3208f387916b8
barcelosrui <barcelos.rui@gmail.com>
2017-05-30 23:19:34 +0100

    Who Am i

--- src/app/signIn/signIn-teacher.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-	templateUrl: "./signIn-teacher.component.html",
-})
-
-export class SignInTeacherComponent{
-  title = 'Registar Professor';
-}
