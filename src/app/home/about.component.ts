4f382830c56ff7d8a64616e7433eb3fc2a251f04
davidbernardo <ravageiro@hotmail.com>
2017-06-02 00:52:16 +0100

    Routes, begining of the implementation of the about us and contacts pages

---
+++ src/app/home/about.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core"
+@Component({
+  templateUrl: "./about.component.html"
+})
+export class AboutComponent{
+    title = 'About Us';
+    saudacao = 'Bem vindo à Base de Dados de Projetos Finais'
+}
